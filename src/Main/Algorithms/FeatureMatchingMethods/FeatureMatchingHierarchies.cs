11fa6b5f90d5ff3b947abeb2f40b43cdfd527dbf
dwgoldbe <daniel.goldberg@tamu.edu>
2015-06-18 15:39:03 -0500

    Initial Commit

---
+++ src/Main/Algorithms/FeatureMatchingMethods/FeatureMatchingHierarchies.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data;
+
+namespace USC.GISResearchLab.Common.Core.Geocoders.FeatureMatching
+{
+    public enum FeatureMatchingSelectionMethod { FeatureClassBased, UncertaintySingleFeatureArea, UncertaintyMultiFeatureGraviational, UncertaintyMultiFeatureTopological };
+
+    public enum FeatureMatchingHierarchyOrdering { NAACCR, NAACCRReversed };
+
+    [Serializable]
+    public class FeatureMatchingHierarchies
+    {
+
+        public static DataTable GetAllFeatureMatchingSelectionMethods()
+        {
+            DataTable ret = new DataTable();
+            ret.Columns.Add(new DataColumn("value", typeof(string)));
+
+            foreach (FeatureMatchingSelectionMethod item in Enum.GetValues(typeof(FeatureMatchingSelectionMethod)))
+            {
+                DataRow row = ret.NewRow();
+                row["value"] = item.ToString();
+                ret.Rows.Add(row);
+            }
+
+            return ret;
+        }
+
+    }
+}
